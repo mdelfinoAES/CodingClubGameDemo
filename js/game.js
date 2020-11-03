78a0fcee955252165f526bf32cd7669237b2b44b
Michael Delfino <mdelfino@aes.ac.in>
2020-11-03 16:32:48 +0530

    Add collision detection and a goal

--- js/game.js
+++ js/game.js
@@ -6,6 +6,12 @@ let player = {
   score: 0
 };
 
+let goal = {
+  x: 400,
+  y: 400,
+  size: 25
+}
+
 function setup() {
   createCanvas(500, 500);
 }
@@ -16,8 +22,25 @@ function draw() {
 
   textSize(24);
   text(player.score, 25, 25);
+
+  noStroke();
+
+  //draw goal
+  fill(200, 200, 50);
+  circle(goal.x, goal.y, goal.size);
+
+  //draw player
+  fill(50, 200, 50);
   circle(player.x, player.y, player.size);
 
+  //check to see if player reached goal
+  if (collides(player, goal)) {
+    player.score += 1;
+    goal.x = random(width);
+    goal.y = random(height);
+  }
+
+  //player input
   if (keyIsDown(UP_ARROW)) {
     player.y -= player.speed;
   }
@@ -29,5 +52,22 @@ function draw() {
   }
   if (keyIsDown(LEFT_ARROW)) {
     player.x -= player.speed;
+  }
+}
+
+function distance(obj1, obj2) {
+  let dx = obj1.x - obj2.x;
+  let dy = obj1.y - obj2.y;
+  return Math.sqrt(dx*dx + dy*dy);
+}
+
+function collides(obj1, obj2) {
+  let d = distance(obj1, obj2);
+  let r1 = obj1.size / 2;
+  let r2 = obj2.size / 2;
+  if (d < r1 + r2) {
+    return true;
+  } else {
+    return false;
   }
 }
