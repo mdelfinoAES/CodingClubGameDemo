08460e086e55cf37155fb808a50b3ab0cd7eb1f6
Michael Delfino <mdelfino@aes.ac.in>
2020-11-03 16:41:08 +0530

    Add enemy and game over screen

--- js/game.js
+++ js/game.js
@@ -12,6 +12,15 @@ let goal = {
   size: 25
 }
 
+let enemy = {
+  x: 500,
+  y:0,
+  size: 40,
+  speed: 2
+}
+
+let gameOver = false;
+
 function setup() {
   createCanvas(500, 500);
 }
@@ -19,39 +28,68 @@ function setup() {
 function draw() {
   background(50, 50, 200);
   fill(255);
+  if (!gameOver) {
+    textSize(24);
+    text(player.score, 25, 25);
 
-  textSize(24);
-  text(player.score, 25, 25);
+    noStroke();
 
-  noStroke();
+    //draw goal
+    fill(200, 200, 50);
+    circle(goal.x, goal.y, goal.size);
 
-  //draw goal
-  fill(200, 200, 50);
-  circle(goal.x, goal.y, goal.size);
+    //draw player
+    fill(50, 200, 50);
+    circle(player.x, player.y, player.size);
 
-  //draw player
-  fill(50, 200, 50);
-  circle(player.x, player.y, player.size);
+    //draw enemy
+    fill(200, 50, 50);
+    circle(enemy.x, enemy.y, enemy.size);
 
-  //check to see if player reached goal
-  if (collides(player, goal)) {
-    player.score += 1;
-    goal.x = random(width);
-    goal.y = random(height);
-  }
+    //check to see if player reached goal
+    if (collides(player, goal)) {
+      player.score += 1;
+      goal.x = random(width);
+      goal.y = random(height);
+    }
 
-  //player input
-  if (keyIsDown(UP_ARROW)) {
-    player.y -= player.speed;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    player.y += player.speed;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    player.x += player.speed;
-  }
-  if (keyIsDown(LEFT_ARROW)) {
-    player.x -= player.speed;
+    //check if enemy got player
+    if (collides(player, enemy)) {
+      gameOver = true;
+    }
+
+    //make enemy chase player
+    if (enemy.x < player.x) {
+      enemy.x += enemy.speed;
+    }
+    if (enemy.x > player.x) {
+      enemy.x -= enemy.speed;
+    }
+    if (enemy.y < player.y) {
+      enemy.y += enemy.speed;
+    }
+    if (enemy.y > player.y) {
+      enemy.y -= enemy.speed;
+    }
+
+    //player input
+    if (keyIsDown(UP_ARROW)) {
+      player.y -= player.speed;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      player.y += player.speed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      player.x += player.speed;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+      player.x -= player.speed;
+    }
+  } else {
+    background(0);
+    fill(255);
+    text("GAME OVER", 180, 200);
+    text("Final score: " + player.score, 180, 250);
   }
 }
 
